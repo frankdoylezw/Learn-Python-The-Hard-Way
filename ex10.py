595b311a2a75de6559550d7a6d067d8f4648a291
frankdoylezw <frankdoylezw@gmail.com>
2016-11-04 19:57:25 +0000

    Adding comments to my ex10 code

--- ex10.py
+++ ex10.py
@@ -1,13 +1,23 @@
+#We're declaring a load of variables up here at the top, again.
+#In this first line, we see the tabbing escape character.
 tabby_cat = "\tI'm tabbed in."
+#In the next line we see the new line escape character
 persian_cat = "I'm split \non a line"
+#Here we see the ability to still include a slash in the text, by escaping with another slash
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+\r
+\r
+\r
+
+\t\tYO YO YO
+\v
+'''
 print tabby_cat
 print persian_cat
 print backslash_cat
